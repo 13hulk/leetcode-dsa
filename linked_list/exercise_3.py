7748e8ed481871c4994d5f74b6712ab92f1f9c64
13hulk <vikas.zingade@gmail.com>
2024-06-28 01:55:08 +0530

    [LinkedList] Add pop method

--- linked_list/exercise_3.py
+++ linked_list/exercise_3.py
@@ -30,13 +30,35 @@ class LinkedList:
 
         if self.head is None:
             self.head = _new_node
+            self.tail = _new_node
         else:
             self.tail.next = _new_node
+            self.tail = _new_node
 
-        self.tail = _new_node
         self.length += 1
 
         return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+
+        _temp = self.head
+        _new_tail = _temp
+
+        while _temp.next is not None:
+            _new_tail = _temp
+            _temp = _temp.next
+
+        self.tail = _new_tail
+        _new_tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return _temp.value
 
 
 if __name__ == "__main__":
@@ -44,4 +66,12 @@ if __name__ == "__main__":
     ll.print_linked_list()
 
     ll.append(10)
+    ll.append(11)
+    ll.append(12)
+    ll.append(13)
+    ll.append(14)
+    ll.append(15)
+    ll.print_linked_list()
+
+    gll.pop()
     ll.print_linked_list()
